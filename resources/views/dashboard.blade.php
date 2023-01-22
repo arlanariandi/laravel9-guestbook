ee6b835ff1efa6c58ef402a3260035680c3da222
Arlan Ariandi Trislis <arlanariandi@gmail.com>
2023-01-22 13:28:38 +0700

    UPDATE: Update view dashboard role user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,86 +19,88 @@
                     </div>
                 </div>
 
-                {{-- content --}}
-                <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-3">
-                    {{-- today's guest --}}
-                    <div class="p-6">
-                        <div class="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-8 h-8 text-gray-400"
-                                viewBox="0 0 16 16">
-                                <path
-                                    d="M4.684 12.523v-2.3h2.261v-.61H4.684V7.801h2.464v-.61H4v5.332h.684zm3.296 0h.676V9.98c0-.554.227-1.007.953-1.007.125 0 .258.004.329.015v-.613a1.806 1.806 0 0 0-.254-.02c-.582 0-.891.32-1.012.567h-.02v-.504H7.98v4.105zm2.805-5.093c0 .238.192.425.43.425a.428.428 0 1 0 0-.855.426.426 0 0 0-.43.43zm.094 5.093h.672V8.418h-.672v4.105z" />
-                                <path
-                                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
-                                <path
-                                    d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z" />
-                            </svg>
-                            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
-                                Tamu Hari Ini
+                @if (Auth::user()->roles == 'ADMIN')
+                    {{-- content --}}
+                    <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-3">
+                        {{-- today's guest --}}
+                        <div class="p-6">
+                            <div class="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                    class="w-8 h-8 text-gray-400" viewBox="0 0 16 16">
+                                    <path
+                                        d="M4.684 12.523v-2.3h2.261v-.61H4.684V7.801h2.464v-.61H4v5.332h.684zm3.296 0h.676V9.98c0-.554.227-1.007.953-1.007.125 0 .258.004.329.015v-.613a1.806 1.806 0 0 0-.254-.02c-.582 0-.891.32-1.012.567h-.02v-.504H7.98v4.105zm2.805-5.093c0 .238.192.425.43.425a.428.428 0 1 0 0-.855.426.426 0 0 0-.43.43zm.094 5.093h.672V8.418h-.672v4.105z" />
+                                    <path
+                                        d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
+                                    <path
+                                        d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z" />
+                                </svg>
+                                <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
+                                    Tamu Hari Ini
+                                </div>
+                            </div>
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-5xl font-semibold text-gray-500">
+                                    {{ $countToday }}<span class="text-lg">tamu</span>
+                                </div>
                             </div>
                         </div>
 
-                        <div class="ml-12">
-                            <div class="mt-2 text-5xl font-semibold text-gray-500">
-                                {{ $countToday }}<span class="text-lg">tamu</span>
+                        {{-- guest last month --}}
+                        <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
+                            <div class="flex items-center">
+
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                    class="w-8 h-8 text-gray-400" viewBox="0 0 16 16">
+                                    <path
+                                        d="m2.56 12.332.54-1.602h1.984l.54 1.602h.718L4.444 7h-.696L1.85 12.332h.71zm1.544-4.527L4.9 10.18H3.284l.8-2.375h.02zm5.746.422h-.676v2.543c0 .652-.414 1.023-1.004 1.023-.539 0-.98-.246-.98-1.012V8.227h-.676v2.746c0 .941.606 1.425 1.453 1.425.656 0 1.043-.28 1.188-.605h.027v.539h.668V8.227zm2.258 5.046c-.563 0-.91-.304-.985-.636h-.687c.094.683.625 1.199 1.668 1.199.93 0 1.746-.527 1.746-1.578V8.227h-.649v.578h-.019c-.191-.348-.637-.64-1.195-.64-.965 0-1.64.679-1.64 1.886v.34c0 1.23.683 1.902 1.64 1.902.558 0 1.008-.293 1.172-.648h.02v.605c0 .645-.423 1.023-1.071 1.023zm.008-4.53c.648 0 1.062.527 1.062 1.359v.253c0 .848-.39 1.364-1.062 1.364-.692 0-1.098-.512-1.098-1.364v-.253c0-.868.406-1.36 1.098-1.36z" />
+                                    <path
+                                        d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
+                                    <path
+                                        d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z" />
+                                </svg>
+                                <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
+                                    Tamu Bulan Lalu
+                                </div>
+                            </div>
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-5xl text-gray-500">
+                                    {{ $countLastMonth }}<span class="text-lg">tamu</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        {{-- all guests --}}
+                        <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
+                            <div class="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                    class="w-8 h-8 text-gray-400" viewBox="0 0 16 16">
+                                    <path
+                                        d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+                                    <path
+                                        d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
+                                    <path
+                                        d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z" />
+                                </svg>
+                                <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
+                                    Semua Tamu
+                                </div>
+                            </div>
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-5xl text-gray-500">
+                                    {{ $countAll }}<span class="text-lg">tamu</span>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    {{-- guest last month --}}
-                    <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
-                        <div class="flex items-center">
-
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-8 h-8 text-gray-400"
-                                viewBox="0 0 16 16">
-                                <path
-                                    d="m2.56 12.332.54-1.602h1.984l.54 1.602h.718L4.444 7h-.696L1.85 12.332h.71zm1.544-4.527L4.9 10.18H3.284l.8-2.375h.02zm5.746.422h-.676v2.543c0 .652-.414 1.023-1.004 1.023-.539 0-.98-.246-.98-1.012V8.227h-.676v2.746c0 .941.606 1.425 1.453 1.425.656 0 1.043-.28 1.188-.605h.027v.539h.668V8.227zm2.258 5.046c-.563 0-.91-.304-.985-.636h-.687c.094.683.625 1.199 1.668 1.199.93 0 1.746-.527 1.746-1.578V8.227h-.649v.578h-.019c-.191-.348-.637-.64-1.195-.64-.965 0-1.64.679-1.64 1.886v.34c0 1.23.683 1.902 1.64 1.902.558 0 1.008-.293 1.172-.648h.02v.605c0 .645-.423 1.023-1.071 1.023zm.008-4.53c.648 0 1.062.527 1.062 1.359v.253c0 .848-.39 1.364-1.062 1.364-.692 0-1.098-.512-1.098-1.364v-.253c0-.868.406-1.36 1.098-1.36z" />
-                                <path
-                                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
-                                <path
-                                    d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z" />
-                            </svg>
-                            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
-                                Tamu Bulan Lalu
-                            </div>
-                        </div>
-
-                        <div class="ml-12">
-                            <div class="mt-2 text-5xl text-gray-500">
-                                {{ $countLastMonth }}<span class="text-lg">tamu</span>
-                            </div>
-                        </div>
+                    {{-- chart --}}
+                    <div class="m-12">
+                        <canvas id="myChart"></canvas>
                     </div>
-
-                    {{-- all guests --}}
-                    <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
-                        <div class="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-8 h-8 text-gray-400"
-                                viewBox="0 0 16 16">
-                                <path
-                                    d="M10.854 8.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
-                                <path
-                                    d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
-                                <path
-                                    d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z" />
-                            </svg>
-                            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">
-                                Semua Tamu
-                            </div>
-                        </div>
-
-                        <div class="ml-12">
-                            <div class="mt-2 text-5xl text-gray-500">
-                                {{ $countAll }}<span class="text-lg">tamu</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                {{-- chart --}}
-                <div class="m-12">
-                    <canvas id="myChart"></canvas>
-                </div>
+                @endif
 
                 {{-- chart js --}}
                 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
